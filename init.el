20f64750d7303e4d677f5687fd73e307aa03bfcc
Eric Thivierge <thiveri@gmail.com>
2013-01-14 16:19:18 -0500

    +.gitignore; +modes; modif init.el

--- init.el
+++ init.el
@@ -14,8 +14,29 @@
 (add-hook 'scheme-mode-hook (function gambit-mode))
 (setq scheme-program-name "gsi -:t")
 
-;; ambit scheme
-;;(require 'gambit)
+;; gambit scheme
+(require 'gambit)
+
+;; Py
+(add-hook 'python-mode-hook
+      (lambda ()
+        (setq indent-tabs-mode t)
+        (setq tab-width 4)
+        (setq python-indent 4)))
+
+(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+(setq prolog-system 'swi)
+(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+                                ("\\.m$" . mercury-mode))
+                              auto-mode-alist))
+(require 'prolog)
+
+;; markdown
+(autoload 'markdown-mode "markdown-mode/markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
 
 ;; Emacs server
 (server-start)
@@ -35,7 +56,11 @@
 ;; Syntax highlighting
 (global-font-lock-mode 1)
 
+;; Cursor color
+(set-cursor-color "Black")
+
 ;; General settings
+;; http://homepages.inf.ed.ac.uk/s0243221/emacs/
 (setq user-full-name "Eric Thivierge"
       user-mail-address "thiveri@gmail.com"
       inhibit-startup-message t
@@ -52,13 +77,25 @@
       echo-keystrokes 0.1
       x-select-enable-clipboard t
       custom-unlispify-tag-names nil
-      ring-bell-function '(lambda ()))
+      ring-bell-function '(lambda ())
+      mouse-wheel-mode t
+      make-backup-files t
+      version-control t
+      backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
 
 (setenv "EDITOR" "emacsclient")
+
+(add-to-list 'load-path "/opt/local/share/emacs/site-lisp/color-theme-6.6.0")
+(require 'color-theme)
+(eval-after-load "color-theme"
+                 '(progn
+                   (color-theme-initialize)
+                   (color-theme-hober)))
 
 ;; Indentation is 4 spaces.
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(setq-default c-basic-offset 4)
 
 ;; Display line, column and time (24h format)
 (line-number-mode t)
@@ -72,7 +109,13 @@
 ;; Scheme
 (setq scheme-program-name "gsi")
 
-;; My custom keybindings
+;; My macros
+
+;; Insert java "System.out.println("");" and move in the quotes
+(fset 'sop
+   [?S ?y ?s ?t ?e ?m ?. ?o ?u ?t ?. ?p ?r ?i ?n ?t ?l ?n ?\( ?\" ?\" ?\) ?\; left left left])
+
+; My custom keybindings
 
 (global-set-key (kbd "<home>") 'beginning-of-line)
 (global-set-key (kbd "<end>") 'end-of-line)
@@ -90,7 +133,7 @@
 (global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-x m") '(lambda ()
                                  (interactive)
-                                 (if (buffer-exists "*ansi-term*")
+                                 (if (get-buffer "*ansi-term*")
                                      (switch-to-buffer-other-window "*ansi-term*")
                                    (ansi-term (getenv "SHELL")))))
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -127,3 +170,18 @@
     (load
      (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-time-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
